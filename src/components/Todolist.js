b556c8a6050e99723e5732df737d1f9374c00e90
Ash052006 <aishwarysaxena2006@gmail.com>
2025-07-18 02:19:25 +0530

    downloaded bootstrap, added button and center aligned

---
+++ src/components/Todolist.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Todolist = () => {
+    return (
+        <>
+            <div className='header text-center'>
+                <h3>Todo List</h3>
+                <button className="btn btn-primary">Create Task</button>
+            </div>
+            <div className='task-container'>
+
+            </div>
+        </>
+    );
+};
+
+export default Todolist;
