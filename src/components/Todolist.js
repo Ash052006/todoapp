d472792b0dac50bb71294b66c78aaf833ebd9c6f
Ash052006 <aishwarysaxena2006@gmail.com>
2025-07-18 19:06:32 +0530

    first part done with all bugs fixed

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Createtask from '../modals/createtask';
 const Todolist = () => {
     const [modal, setmodal]=useState(false);
     const [taskList, setTaskList]= useState([])
+
+    useEffect(()=>{
+        let arr=localStorage.getItem("taskList")
+        let obj= JSON.parse(arr)
+        if(arr){
+            setTaskList(obj)
+        }
+    },[])
     const toggle =() => {
         setmodal(!modal);
     }
     const saveTask=(taskObj)=>{
         let tempList=taskList
         tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
         setTaskList(tempList)
         setmodal(false)
     }
@@ -19,7 +28,7 @@ const Todolist = () => {
                 <button className="btn btn-primary mt-2" onClick={() => setmodal(true)}>Create Task</button>
             </div>
             <div className='task-container'>
-                {taskList.map(()=><li>{Object.name}</li>)}
+                {taskList.map((obj)=><li>{obj.Name}</li>)}
             </div>
             <Createtask toggle={toggle} modal = {modal} save={saveTask}/> 
         </>
