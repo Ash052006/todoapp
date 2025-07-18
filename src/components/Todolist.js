7551bd66f1087a2add34a25dc3d0901d189a26d5
Ash052006 <aishwarysaxena2006@gmail.com>
2025-07-18 16:25:11 +0530

    popup added

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,15 +1,20 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Createtask from '../modals/createtask';
 const Todolist = () => {
+    const [modal, setmodal]=useState(false);
+    const toggle =() => {
+        setmodal(!modal);
+    }
     return (
         <>
             <div className='header text-center'>
                 <h3>Todo List</h3>
-                <button className="btn btn-primary">Create Task</button>
+                <button className="btn btn-primary mt-2" onClick={() => setmodal(true)}>Create Task</button>
             </div>
             <div className='task-container'>
 
             </div>
+            <Createtask toggle={toggle} modal = {modal} /> 
         </>
     );
 };
